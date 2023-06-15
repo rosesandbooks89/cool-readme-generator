7eb22ab38b382b6443666dd858e536020d0836ec
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-06-15 15:21:41 -0500

    updating repo to work with tutor

--- index.js
+++ index.js
@@ -1,13 +1,147 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
+
+
+
+
+const generateReadMe = ({ProjectTitle, Description, Installation, Usage, License, Contribution, Test, username, profile}) =>
+`# ${ProjectTitle}
+
+//table of contents with links
+## Table of Contents
+-[description](Description)
+-[installation](Installation)
+-[usage](Usage)
+-[license](Licenses)
+-[contribution](Contribution)
+-[test](Test)
+
+
+## Description
+${Description}
+
+## Installation
+${Installation}
+
+## Usage
+${Usage}
+
+## Licenses
+${License}
+
+## Contribution
+${Contribution}
+
+## Test
+${Test}
+
+## Questions?
+'github.com/'+ ${username}
+If you have any questions please email me at: + ${email}.
+
+
+
+
+
+
+
+
+`
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'projectTitle',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("You must choose a title.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: 'How would you describe your project?',
+            name: 'description',
+            validate: function (answer) {    
+                if (answer.length < 1) {
+                    return console.log("You need add a description!!!");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'If applicable, Describe the usage of your project.',
+            name: 'usage',
+        }, 
+        {
+            type: 'checkbox',
+            message: 'Did you use an license for your project?',
+            name: 'license',
+            choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+            badge: ['https://img.shields.io/badge/License-MIT-yellow.svg', 'https://img.shields.io/badge/License-Apache%202.0-blue.svg', 'https://img.shields.io/badge/License-GPLv3-blue.svg', 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg', 'https://img.shields.io/badge/License-None-blue.svg'],
+        },
+        {
+            type: 'input',
+            message: 'If applicable, Were there any contributors to your project?',
+            name: 'contributing',
+            
+        },
+        { type: 'input',
+        message: 'If applicable, provide any tests written for your application and provide examples on how to run them.',
+        name: 'tests',
+    },
+    //github username link
+    //email address link with how to reach me with additional questions
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = () => {
+    questions()
+    .then((answers) => {
+        const genRead = generateReadMe(answers);
+        console.log(genRead);
+        fs.writeFile('README.md', genRead, (err) =>
+              err ? console.log(err) : console.log('Success!'))
+    });
+    }    
+
+
+
+writeToFile();
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () =>{
+    
+
+}
 
 // Function call to initialize app
 init();
+
+
+//add badges to license
+
+
