4c3f1e676cdbf01f713a32386d31bb04bba8cbff
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-06-20 01:23:28 -0500

    updating repo

--- README.md
+++ README.md
@@ -1,34 +1,40 @@
-# **k**
+# **Awesome README Maker!**
 
-//table of contents with links
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+
 ## **Table of Contents**
-* [Description](Description)
-* [Installation](Installation)
-* [Usage](Usage)
-* [License](Licenses)
-* [Contribution](Contribution)
-* [Test](Tests)
-* [Questions](Questions)
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#licenses)
+* [Contribution](#contribution)
+* [Test](#tests)
+* [Questions?](#questions)
 
 
-###Description
-k
+## Description
+This is cool app that builds a README with the answers you provide to the following Questions. 
 
-###Installation
-k
+## Installation
+ Open a terminal window, type in "node index" and answer the questions. Once done the application will generate the README.md file.
 
-###Usage
-k
-
-###Licenses
-MIT
-
-###Contribution
-undefined
-
-###Tests
+## Usage
 
 
-###Questions?
-Here is a link to my github. This site was built using [GitHub Repo](https://github.comrosesandbooks89).
-If you have any questions please email me at: + rosesandbooks89@gmail.com.
+## License
+
+Awesome README Maker! is licensed under the Apache License.
+
+
+## Contribution
+None at this time. 
+
+## Tests
+No tests
+
+## Questions?
+Here is a link to my github. [GitHub Repo](https://github.com/rosesandbooks89).
+
+If you have any questions please email me at: rosesandbooks89@gmail.com.
--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-
+//generates badge and link for license section
+const renderLicense = (license) => {
+  if (license == "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license == "Apache") {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  } else if (license == "BSD 3-Clause License") {
+    return "[[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+  } else if (license == "None") {
+    return "No Licenses were used.";
+  } else {
+    return "";
+  }
+};
 const generateReadMe = ({
-    projectTitle, 
-    description, 
-    installation, 
-    usage, 
-    license, 
-    contribution, 
-    tests,
-    username, 
-    email
+  projectTitle,
+  description,
+  installation,
+  usage,
+  license,
+  contribution,
+  tests,
+  username,
+  email,
 }) =>
+  `# **${projectTitle}**
 
-`# **${projectTitle}**
+${renderLicense(license)}
 
-//table of contents with links
+
 ## **Table of Contents**
-* [Description](Description)
-* [Installation](Installation)
-* [Usage](Usage)
-* [License](Licenses)
-* [Contribution](Contribution)
-* [Test](Tests)
-* [Questions](Questions)
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#licenses)
+* [Contribution](#contribution)
+* [Test](#tests)
+* [Questions?](#questions)
 
 
 ## Description
@@ -36,8 +51,10 @@ ${installation}
 ## Usage
 ${usage}
 
-## Licenses
-${license}
+## License
+
+${projectTitle} is licensed under the ${license} License.
+
 
 ## Contribution
 ${contribution}
@@ -46,90 +63,94 @@ ${contribution}
 ${tests}
 
 ## Questions?
-Here is a link to my github. This site was built using [GitHub Repo](https://github.com${username}).
-If you have any questions please email me at: + ${email}.
-`
+Here is a link to my github. [GitHub Repo](https://github.com/${username}).
+
+If you have any questions please email me at: ${email}.
+`;
 // TODO: Create an array of questions for user input
 const questions = () => {
-    return inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'projectTitle',
-            validate: function (answer) {
-                if (answer.length < 1) {
-                    return console.log("Wait!! You must choose a title.");
-                }
-                return true;
-            }
-        },
-        {
-            type: 'input',
-            message: 'How would you describe your project?',
-            name: 'description',
-            validate: function (answer) {    
-                if (answer.length < 1) {
-                    return console.log("You need add a description!!!");
-                }
-                return true;
-            }
-        },
-        {
-            type: 'input',
-            message: 'What are the steps required to install your project?',
-            name: 'installation',
-        },
-        {
-            type: 'input',
-            message: 'If applicable, Describe the usage of your project.',
-            name: 'usage',
-        }, 
-        {
-            type: 'checkbox',
-            message: 'Did you use an license for your project?',
-            name: 'license',
-            choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
-            badge: ['https://img.shields.io/badge/License-MIT-yellow.svg', 'https://img.shields.io/badge/License-Apache%202.0-blue.svg', 'https://img.shields.io/badge/License-GPLv3-blue.svg', 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg', 'https://img.shields.io/badge/License-None-blue.svg'],
-        },
-        {
-            type: 'input',
-            message: 'If applicable, Were there any contributors to your project?',
-            name: 'contributing',
-            
-        },
-        { type: 'input',
-        message: 'If applicable, provide any tests written for your application and provide examples on how to run them.',
-        name: 'tests',
-    },
-    //github username link
-    //email address link with how to reach me with additional questions
+  return inquirer.prompt([
     {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'username',
+      type: "input",
+      message: "What is the title of your project?",
+      name: "projectTitle",
+      validate: function (answer) {
+        if (answer.length < 1) {
+          return console.log("Wait!! You must choose a title.");
+        }
+        return true;
+      },
     },
     {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
+      type: "input",
+      message: "How would you describe your project?",
+      name: "description",
+      validate: function (answer) {
+        if (answer.length < 1) {
+          return console.log("You need add a description!!!");
+        }
+        return true;
+      },
     },
-])
-}
+    {
+      type: "input",
+      message: "What are the steps required to install your project?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "If applicable, Describe the usage of your project.",
+      name: "usage",
+    },
+    {
+      type: "checkbox",
+      message: "Did you use an license for your project?",
+      name: "license",
+      choices: ["MIT", "Apache", "MPL 2.0", "BSD 3-Clause License", "None"],
+      validate: function (answer) {
+        if (answer.length < 1) {
+          return console.log("Wait!! You must choose an option");
+        }
+        return true;
+      },
+    },
+    {
+      type: "input",
+      message: "If applicable, Were there any contributors to your project?",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message:
+        "If applicable, provide any tests written for your application and provide examples on how to run them.",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 const writeToFile = () => {
-    questions()
-    .then((answers) => {
-        const genRead = generateReadMe(answers);
-        console.log(genRead);
-        fs.writeFile('README.md', genRead, (err) =>
-              err ? console.log(err) : console.log('Thank you for answering the questins. Your README.md file has been generated!'))
-    });
-    }    
+  questions().then((answers) => {
+    const genRead = generateReadMe(answers);
+    console.log(genRead);
+    fs.writeFile("README.md", genRead, (err) =>
+      err
+        ? console.log(err)
+        : console.log(
+            "Thank you for answering the questins. Your README.md file has been generated!"
+          )
+    );
+  });
+};
 
 writeToFile();
-
-//add badges to license
-
-
